a5533650295c7b057a5195c243216e3dc61778f1
jangcla <jangcla@ymail.com>
2021-11-10 23:38:54 -0800

    pushing up the first attempt

--- problems/longest-non-repeating-substring.js
+++ problems/longest-non-repeating-substring.js
@@ -1,3 +1,22 @@
 // Create a method that takes in a string and returns the longest length of a non repeating substring
 
 //1st attempt
+const longestNonRepeatingSubstring = function (string) {
+    if (string === '') return 0;
+    let seen = [];
+    let longest = [];
+
+    for (let i = 0; i < string.length; i++) {
+        let char = string[i]
+        if (!seen.includes(char)) {
+            seen.push(char);
+        } else {
+            longest.push(seen.join(""));
+            seen = [];
+            seen.push(char)
+        }
+    }
+
+    let longestWord = longest.sort(function (a, b) { return b.length - a.length; });
+    return longestWord[0].length
+};
