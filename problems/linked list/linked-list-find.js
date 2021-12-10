b56d9a0e5d7e3a7ffde8757bd94eba7e6b4e786f
jangcla <jangcla@ymail.com>
2021-12-09 19:34:14 -0800

    adding in the first attemt

--- problems/linked list/linked-list-find.js
+++ problems/linked list/linked-list-find.js
@@ -1,3 +1,22 @@
 // linked list find
 // Write a function, linkedListFind, that takes in the head of a linked list and a target value.The function should return a boolean indicating whether or not the linked list contains the target.
 
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+
+// ATTEMP 1
+// const linkedListFind = (head, target) => {
+//   let current = head;
+
+//   while (current !== null) {
+//     if (current.val === target) return true;
+
+//     current = current.next;
+//   }
+
+//   return false;
+// };
