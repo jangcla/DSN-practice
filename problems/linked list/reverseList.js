d796281cece6f69d115bc93bc6f49cc69d367f64
jangcla <jangcla@ymail.com>
2021-12-29 23:02:09 -0800

    pushing up a recursive approach to the problem

--- problems/linked list/reverseList.js
+++ problems/linked list/reverseList.js
@@ -24,3 +24,17 @@
 
 // this is time: O(n) as we itterate through the linked list
 // this is space: O(1) as we initialize the value and just override it as we itterate
+
+const reverseList = (head, prev = null) => {
+    if (head === null) return prev;
+
+    let next = head.next;
+
+    head.next = prev;
+    return reverseList(next, head)
+}
+
+// this is time: O(n) as we recursion through the whole list
+// this is space: O(n) as every stack stores the relevant data until we stop recursioning through
+
+
