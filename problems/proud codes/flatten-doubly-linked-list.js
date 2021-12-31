40c1e7ff8429d756630510faa4e199d4346c6770
jangcla <jangcla@ymail.com>
2021-12-30 21:08:40 -0800

    pushing up the code with time: O(n) and space: O(1)

--- problems/proud codes/flatten-doubly-linked-list.js
+++ problems/proud codes/flatten-doubly-linked-list.js
@@ -4,3 +4,45 @@
 
 // Return the head of the flattened list.The nodes in the list must have all of their child pointers set to null.
 
+/**
+ * // Definition for a Node.
+ * function Node(val,prev,next,child) {
+ *    this.val = val;
+ *    this.prev = prev;
+ *    this.next = next;
+ *    this.child = child;
+ * };
+ */
+
+/**
+ * @param {Node} head
+ * @return {Node}
+ */
+// need to itterate through the linked list 
+//  if a child pointer is found, traverse through the child linked list first
+//   store the original next pointer to not lose the data
+var flatten = function (head) {
+    if (head === null) return null;
+
+    let current = head;
+    let temporary = [];
+
+    while (current !== null) {
+        if (current.child !== null) {
+            if (current.next !== null) temporary.push(current.next);
+            current.next = current.child;
+            current.child.prev = current;
+        }
+
+        if (current.next === null && temporary.length !== 0) {
+            let next = temporary.pop();
+            current.next = next;
+            next.prev = current;
+
+        }
+
+        current.child = null;
+        current = current.next;
+    }
+    return head;
+};
