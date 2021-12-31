939525e4a1d4102913653680885d9c345f5cfc4b
jangcla <jangcla@ymail.com>
2021-12-30 21:04:33 -0800

    pushing up second attempt at zipper list

--- problems/linked list/zipperList.js
+++ problems/linked list/zipperList.js
@@ -27,3 +27,22 @@
 // this is time: O(n) as we itterate through the linked list only once;
 // this is space: O(1) as we really just change out the next pointer on the linked list;
 
+// ATTEMPT 2
+
+// const zipperLists = (head1, head2) => {
+//     if (head1 === null && head2 === null) return null;
+//     if (head1 === null) return head2;
+//     if (head2 === null) return head1;
+
+//   let next1 = head1.next;
+//   let next2 = head2.next;
+//   head1.next = head2;
+//   head2.next = zipperLists(next1, next2);
+
+//   return head1;
+// }
+
+// this is time: O(n) as we recurssion through the linked list;
+// this is space: O(n) as we store the values in every recursive stack;
+
+
