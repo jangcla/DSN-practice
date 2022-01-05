584b196fe49528f003d03649732e1ac67d8c08fb
jangcla <jangcla@ymail.com>
2022-01-04 23:05:28 -0800

    adding on second and final attempt with time and space notes at the end

--- problems/linked list/longest-streak.js
+++ problems/linked list/longest-streak.js
@@ -22,3 +22,28 @@
   
 //   return longest;
 // };
+
+
+//ATTEMPT 2
+const longestStreak = (head) => {
+    let count = 0;
+    let longest = 0;
+
+    let current = head;
+
+    while (current !== null) {
+        count++;
+        if (count > longest) longest = count; //checks if the current set of values streak is greater, then it'll over write the old longest streak
+
+        if (current.next !== null) { //if the next value is not null and the value is not the same as the current value
+            if (current.next.val !== current.val) count = 0; //then we restart the count
+        }
+
+        current = current.next //itterates through the whole linked list;
+    }
+
+    return longest; //returns the overall longest streak of reoccuring values
+};
+
+//attempt 1 & 2 are time: O(n) as they itterate through a linked list;
+//attempt 1 & 2 are space: O(1) as they only store the values initially and simply over write it as we itterate through the linked list;
