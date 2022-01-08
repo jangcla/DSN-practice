98dd1a56a409f7f25332e882f30bd7524353aeb2
jangcla <jangcla@ymail.com>
2022-01-07 23:05:56 -0800

    pushing up second try and cleaner code version of the question as well as personal notes

--- problems/array and string/intersection.js
+++ problems/array and string/intersection.js
@@ -27,3 +27,20 @@
   
 //   return ans;
 // };
+
+const intersection = (a, b) => {
+    // todo
+    let set = new Set(a);
+    let ans = [];
+
+    for (let i = 0; i < b.length; i++) {
+        let num = b[i];
+        if (set.has(num)) ans.push(num);
+    }
+
+    return ans;
+};
+
+// this is time: O(n + m) => O(n) as we have to itterate through the array of b;
+// this is space: O(n) as we use set to check if it has the number from the second array;
+// key here is that look up time in a set and making a set is O(1);
