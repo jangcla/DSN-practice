a4416b4544e0c100537e59f40719c2bcea9e5f55
jangcla <jangcla@ymail.com>
2022-01-07 23:03:42 -0800

    adding in solution as well as time and space complexity personal notes

--- problems/array and string/five-sort.js
+++ problems/array and string/five-sort.js
@@ -3,3 +3,24 @@
 
 // Elements that are not 5 can appear in any order in the output, as long as all 5s are at the end of the array.
 
+const fiveSort = (nums) => {
+    // todo 
+    let i = 0;
+    let j = nums.length - 1;
+
+    while (i < j) {
+        if (nums[i] === 5 && nums[j] !== 5) {
+            nums[i] = nums[j];
+            nums[j] = 5;
+        }
+        if (nums[i] !== 5) i++;
+        if (nums[j] === 5) j--;
+    }
+
+    return nums;
+};
+
+// this is time: O(n) because we have a while loop that itterates through the nums array
+// this is space: O(1) because we are just keying to portions of the array to switch the values
+
+
