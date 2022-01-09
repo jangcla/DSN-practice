30eb2e7b6ab6b54c89d2866277bd3eeca468465e
jangcla <jangcla@ymail.com>
2022-01-08 23:43:22 -0800

    adding in first attempt of the code along with space and time complexity notes

--- problems/array and string/pair-sum.js
+++ problems/array and string/pair-sum.js
@@ -4,3 +4,34 @@
 // Be sure to return the indices, not the elements themselves.
 
 // There is guaranteed to be one such pair that sums to the target.
+
+//ATTEMPT 1 
+// const pairSum = (numbers, targetSum) => {
+//   // todo
+//   let hash = {};
+  
+//   for (let i = 0; i < numbers.length; i++) {
+//     let num = targetSum - numbers[i];
+//     if (hash[num] === undefined) {
+//       hash[num] = [i];
+//     } else {
+//       hash[num].push(i);
+//     }
+//   }
+//   // console.log(hash);
+  
+//   for (let i = 0; i < numbers.length; i++) {
+//     let number = parseInt(numbers[i]);
+//     if (hash[number] !== undefined){
+//       if ((hash[number].length !== 2) && (hash[number][0] !== i)) {
+//         hash[number].push(i);
+//         return hash[number]
+//       } else if (hash[number].length === 2) {
+//         return hash[number];
+//       }      
+//     }
+//   }
+// };
+
+// this is time: O(n + n) => O(n) because the first loop itterates to store the values while the second loops through to the array;
+// this is space: O(n) as we have to store values into the hash which is factored by n amount in the array;
