451a6e9ec3d9b70a19c912f724a5123e919c759d
jangcla <jangcla@ymail.com>
2022-01-17 23:02:12 -0800

    adding in dsa practice of the day "compress

---
+++ problems/array and string/compress.js
@@ -0,0 +1,5 @@
+// compress
+// Write a function, compress, that takes in a string as an argument.The function should return a compressed version of the string where consecutive occurrences of the 
+// same characters are compressed into the number of occurrences followed by the character.Single character occurrences should not be changed.
+
+
