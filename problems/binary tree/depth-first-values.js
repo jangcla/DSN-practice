fe4569583d67c561010207655ff892dcd5cbcd13
jangcla <jangcla@ymail.com>
2022-01-18 23:05:34 -0800

    adding in my itterative approach to the problem

--- problems/binary tree/depth-first-values.js
+++ problems/binary tree/depth-first-values.js
@@ -3,3 +3,23 @@
 
 //     Hey.This is our first binary tree problem, so you should be liberal with watching the Approach and Walkthrough.Be productive, not stubborn. - AZ
 
+const depthFirstValues = (root) => {
+    if (root === null)
+        return [];
+
+    const values = [];
+    const stack = [root];
+
+    while (stack.length > 0) {
+        const node = stack.pop();
+        values.push(node.val);
+
+        if (node.right !== null)
+            stack.push(node.right);
+
+        if (node.left !== null)
+            stack.push(node.left);
+    }
+
+    return values;
+};
