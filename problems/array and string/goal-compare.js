f61a69376567293bb815c7410bc37c347eabcee1
jangcla <jangcla@ymail.com>
2022-02-01 12:12:25 -0800

    added team score comparison DSA practice question

---
+++ problems/array and string/goal-compare.js
@@ -0,0 +1,7 @@
+// There are 2 arrays given from team A and team B. The question asks you to return an array containing how many times team A scored equal to or less than team B.A
+// Ex. teamA = [3,2,4,7,2];
+//     teamB = [5, 3];
+//     output = [4, 3];
+// this is because 5 is equal to or greater than 4 of the scores from team A;
+// this is because 3 is equal to or greater than 3 of the scores from team A;
+// thus resulting in the out put[4, 3];
