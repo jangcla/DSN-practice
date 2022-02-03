619bed47851aec45609020e1a65c981888e563fc
jangcla <jangcla@ymail.com>
2022-02-03 12:12:35 -0800

    added step by step notes explaining how I answered the question

--- problems/array and string/goal-compare.js
+++ problems/array and string/goal-compare.js
@@ -7,29 +7,29 @@
 // thus resulting in the out put[4, 3];
 
 function compare(teamA, teamB) {
-    let ans = [];
+    let ans = []; //this is for the output that asks for an array of number of times teamB scored more or equal to teamA
 
-    let i = 0;
-    let j = 0;
+    let i = 0; //pointer index for team A
+    let j = 0; //pointer index for team B
 
-    let count = 0;
-    while (j < teamB.length) {
+    let count = 0; //counter for the amount of times teamB's score is greater than or equal to teamA's
+    while (j < teamB.length) { //will keep looping until all of TeamB's array has been itterated
         let aScore = teamA[i];
         let bScore = teamB[j];
 
-        if(bScore >= aScore) {
+        if(bScore >= aScore) { //if greater than or equal to, then we increment the count
             count++;
         }
 
-        i++;
-
-        if(i > teamA.length-1) {
-            j++;
-            ans.push(count);
-            count = 0;
-            i = 0;
+        i++; //increments the index for Team A
+ 
+        if(i > teamA.length-1) {  //if i is greater than the length of TeamA array, then we do a couple things
+            j++; //incremeant the index for teamB
+            ans.push(count); //add in the current count to the answer arr
+            count = 0; //restart the count
+            i = 0; //increment into the next element in TeamB's array
         }
     }
 
-    return ans;
+    return ans; //should return an array of all the counts comparing team A and team B
 }
