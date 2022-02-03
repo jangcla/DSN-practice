d801ee87f90372a4fe404543458cf8ab920e7746
jangcla <jangcla@ymail.com>
2022-02-03 12:12:25 -0800

    added team score comparison DSA practice question first attempt initialization

--- problems/array and string/goal-compare.js
+++ problems/array and string/goal-compare.js
@@ -5,3 +5,7 @@
 // this is because 5 is equal to or greater than 4 of the scores from team A;
 // this is because 3 is equal to or greater than 3 of the scores from team A;
 // thus resulting in the out put[4, 3];
+
+function compare(teamA, teamB) {
+    
+}
