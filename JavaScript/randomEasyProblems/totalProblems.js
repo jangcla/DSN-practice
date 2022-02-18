2122585f5f561f138553acb35c6f9a564a03585d
jangcla <jangcla@ymail.com>
2022-02-18 12:12:45 -0800

    total problems easy js problem added (data structuring practice)’

---
+++ JavaScript/randomEasyProblems/totalProblems.js
@@ -0,0 +1,52 @@
+/******************************************************************************
+Write a function totalNumProblems(assessments) that takes in an object of assessment
+objects. The function should return the total number of problems in all assessments.
+
+Example:
+
+var assessments = {
+  w1d5: {
+    totalPoints: 7,
+    problems: ['range', 'reverseSentence', 'unique', 'fizzBuzz', 'stringRange']
+  },
+  w2d1: {
+    totalPoints: 10,
+    problems: ['reverseRange', 'isPrime', 'magicNumbers', 'firstAndLast', 'royalWe']
+  },
+  w2d5: {
+    totalPoints: 7,
+    problems: ['myIndexOf', 'minMaxDifference', 'divisibleBy', 'dynamicFizzBuzz', 'magicCipher']
+  },
+  w3d1: {
+    totalPoints: 7,
+    problems: ['arrayBuilder', 'longestWord', 'leastCommonMultiple', 'sillyCipher', 'hipsterfy']
+  },
+  w3d5: {
+    totalPoints: 5,
+    problems: ['highestScore', 'snakeToCamel', 'sum2DArray', 'minValueCallback', 'mySelect']
+  },
+  w4d1: {
+    totalPoints: 5,
+    problems: ['not', 'so', 'fast']
+  },
+  w4d5: {
+    totalPoints: 4,
+    problems: [':)']
+  }
+}
+
+totalNumProblems(assessments) => 29
+*******************************************************************************/
+
+function totalNumProblems(assessments) {
+    var totalCount = 0;
+
+    for (var key in assessments) {
+        var current = assessments[key];
+        var problemsArr = current.problems;
+
+        totalCount += problemsArr.length;
+    }
+
+    return totalCount;
+}
