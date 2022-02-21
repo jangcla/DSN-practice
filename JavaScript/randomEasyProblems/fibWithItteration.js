073fe9fe6c759abd893ceca17adbea3037753f27
jangcla <jangcla@ymail.com>
2022-02-21 12:12:45 -0800

    fibonacci dsa problem solved in an itterative process’

---
+++ JavaScript/randomEasyProblems/fibWithItteration.js
@@ -0,0 +1,37 @@
+/***********************************************************************
+Write a function fibonacci(n) that takes in a number that represents the
+length of a sequence. The function should should return the first `n`
+numbers in the fibonacci sequence.
+
+The first two numbers of the fibonacci sequence are 1 and 1. The next
+number in the sequence can be found by adding the last two numbers in
+the sequence.
+
+Solve this without recursion.
+If you don't know what recursion is, we'll see it tomorrow!
+
+Examples:
+
+fibonacci(0); // => []
+fibonacci(1); // => [ 1 ]
+fibonacci(2); // => [ 1, 1 ]
+fibonacci(3); // => [ 1, 1, 2 ]
+fibonacci(5); // => [ 1, 1, 2, 3, 5 ]
+fibonacci(10); // => [ 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 ]
+***********************************************************************/
+
+function fibonacci(n) {
+    var fibNums = [];
+
+    for (var i = 0; i < n; i++) {
+        if (i < 2) {
+            fibNums.push(1);
+        } else {
+            var lastValue = fibNums[fibNums.length - 1];
+            var secondLast = fibNums[fibNums.length - 2];
+            fibNums.push(lastValue + secondLast);
+        }
+    }
+
+    return fibNums;
+}
