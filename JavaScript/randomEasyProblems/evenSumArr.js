d755898f96f30253d431927140a4a427eb01bce0
jangcla <jangcla@ymail.com>
2022-02-25 12:12:45 -0800

    sum even adder easy problem’

---
+++ JavaScript/randomEasyProblems/evenSumArr.js
@@ -0,0 +1,32 @@
+/*******************************************************************************
+Write a function evenSumArray(array) that takes in an array of numbers and returns
+a new array where each num is replaced with the sum of all even numbers less than
+or equal to that num.
+
+Examples:
+
+evenSumArray([6, 7, 5]) => [ 12, 12, 6 ]
+evenSumArray([2, 8, 3, 5]) => [ 2, 20, 2, 6 ]
+*******************************************************************************/
+
+function evenSumArray(array) {
+    var newNums = [];
+
+    for (var i = 0; i < array.length; i++) {
+        var current = array[i];
+
+        newNums.push(evenAdder(current));
+    }
+
+    return newNums;
+}
+
+function evenAdder(num) {
+    var evenNums = 0;
+
+    for (var number = 2; number <= num; number += 2) {
+        evenNums += number;
+    }
+
+    return evenNums;
+}
