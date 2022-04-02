35769be4a3089bd46b6fe39786fdc9aafa65f0ec
jangcla <jangcla@ymail.com>
2022-04-02 12:12:45 -0700

    adding in tree sum itterative problem approach’

--- JavaScript/problems/binary tree/treeSum.js
+++ JavaScript/problems/binary tree/treeSum.js
@@ -4,3 +4,23 @@ Write a function, treeSum, that takes in the root of a binary tree that contains
 The function should return the total sum of all values in the tree.
 */
 
+const treeSumItters = (root) => {
+  if (!root) return 0;
+  
+  let sumValues = 0;
+  const stack = [ root ];
+  
+  while (stack.length > 0) {
+    const current = stack.pop();
+    
+    sumValues += current.val;
+    
+    if(current.right) stack.push(current.right);
+    if(current.left) stack.push(current.left);
+  }
+  
+  return sumValues;
+};
+// Itterative Approach
+// this is time: O(n) as it itterates through the loop n amount of times
+// this is space: O(n) as we are continuosly adding to the stack n amount of times
