bc24f261347faf832386bc1dfd0478695ab9d56d
jangcla <jangcla@ymail.com>
2022-04-26 20:57:33 -0700

    added itterative approach to bfs queue solution and complexity notes

--- JavaScript/problems/binary tree/breathFirstValues.js
+++ JavaScript/problems/binary tree/breathFirstValues.js
@@ -6,3 +6,24 @@
 // 2. use a breadth first approach when traversing the tree (queue)
 // 3. return all breadth first values
 
+const breadthFirstValues = (root) => {
+    if (root === null) return [];
+
+    const breadthValues = [];
+    const queue = [root];
+
+    while (queue.length > 0) {
+        let current = queue.shift();
+
+        breadthValues.push(current.val);
+
+        if (current.left !== null) queue.push(current.left);
+        if (current.right !== null) queue.push(current.right);
+    }
+
+    return breadthValues;
+};
+
+// Itterative Approach:
+// Space Complexity: we add n amount of items into the queue and breadthValues so the total space is O(n)
+// Time Complexity: we itterate through the whole tree and that's n while .shift runs in O(n) so n * n is O(n^2);
