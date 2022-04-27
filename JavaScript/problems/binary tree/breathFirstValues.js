a7069cb637c5de3f9d50bc525d8dee2336686137
jangcla <jangcla@ymail.com>
2022-04-26 20:56:37 -0700

    initiated and created the file and approach steps to the bfs approach of binary trees in JS

---
+++ JavaScript/problems/binary tree/breathFirstValues.js
@@ -0,0 +1,8 @@
+// Write a function, breadthFirstValues, that takes in the root of a binary tree.
+// The function should return an array containing all values of the tree in breadth - first order.
+
+// APPROACH
+// 1. need to itteratively grab all values of the tree and store in an array;
+// 2. use a breadth first approach when traversing the tree (queue)
+// 3. return all breadth first values
+
