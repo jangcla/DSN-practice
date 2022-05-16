30a1378e5d9745607bb660dc0a42fae2014ec63f
John Angcla <83146651+jangcla@users.noreply.github.com>
2022-05-16 01:50:58 -0700

    Time: 75 ms (69.34%), Space: 43.8 MB (59.19%) - LeetHub

--- 78-subsets/78-subsets.js
+++ 78-subsets/78-subsets.js
@@ -17,3 +17,11 @@ var subsets = function(nums) {
     
     return subset;
 };
+
+
+// Space complexity: the subset variable has n space and the temp array will have n space as well
+// so this function is => O(n)
+
+// Time Complexity: the function itterates through the numbers array at n time as well as itterate
+// through the current subset that continuosly increases at the runtime of m so we essentially have
+// it as => n * m so O(nm) || O(n^2);
