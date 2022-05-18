8ee17f935d71ad1c5a62405dde7e43b2dc1041e3
John Angcla <83146651+jangcla@users.noreply.github.com>
2022-05-17 18:46:17 -0700

    Time: 95 ms (31.42%), Space: 42.4 MB (48.91%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    if (!s.length) return true;
+    const stack = [s[0]];
+    
+    for (let i = 1; i < s.length; i++) {
+        let current = s[i];
+        let last = stack[stack.length - 1];
+        
+        if (last === ')' || last === "}" || last === "]") return false;
+        
+        // if the current value closes the last value then pop off last val
+        if (last === '(') {
+            if (current === ')') stack.pop();
+            
+            if (current !== ')') stack.push(current);
+        } else if (last === '{') {
+            if (current === '}') stack.pop();
+            
+            if (current !== '}') stack.push(current);
+        } else if (last === '[') {
+            if (current === ']') stack.pop();
+            
+            if (current !== ']') stack.push(current);
+        } else {
+            stack.push(current);
+        }
+        
+        // if the current value doesn't close the last then add on top
+        
+    }
+    
+    return !stack.length
+};
