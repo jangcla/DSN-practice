ad081ff1ee1d2424c4e238754ec960283adcc6e0
John Angcla <83146651+jangcla@users.noreply.github.com>
2022-05-18 19:57:33 -0700

    Time: 160 ms (39.57%), Space: 48.7 MB (74.18%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+    const sorted = intervals.sort((a, b) => { return a[0] - b[0]});
+    const merged = [ intervals[0] ];
+    
+    
+    for (let i = 1; i < sorted.length; i++) {
+        const last = merged[merged.length - 1];
+        const current = sorted[i];
+        
+        if (last[1] >= current[0]) {
+            merged[merged.length - 1][1] = Math.max(last[1], current[1]);
+        } else {
+            merged.push(current);
+        }
+        
+    }
+    
+    return merged;
+};
