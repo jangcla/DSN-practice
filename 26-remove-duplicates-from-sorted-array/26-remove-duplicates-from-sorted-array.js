ce87c5b4795261306521ca59c2f9fc39e6efe739
John Angcla <83146651+jangcla@users.noreply.github.com>
2022-05-22 22:29:53 -0700

    Time: 101 ms (59.13%), Space: 44.8 MB (57.64%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
@@ -5,13 +5,11 @@
 var removeDuplicates = function(nums) {
     const dupes = new Set();
     
-    
     let j = 0;
     for (let i = 0; i < nums.length; i++) {
         const current = nums[i];
         if (dupes.has(current)) continue;
         dupes.add(current);
-        
         
         nums[j] = nums[i];
         j++;
