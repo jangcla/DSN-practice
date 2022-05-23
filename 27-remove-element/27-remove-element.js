ffc6cc83f4b660115b61469ea37f0e6d4396f0c4
John Angcla <83146651+jangcla@users.noreply.github.com>
2022-05-22 22:55:09 -0700

    Time: 84 ms (41.34%), Space: 41.7 MB (94.68%) - LeetHub

---
+++ 27-remove-element/27-remove-element.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let i = 0;
+    let j = nums.length - 1;
+    
+    while (i < j) {
+        if ( nums[i] === val && nums[j] !== val) {
+            nums[i] = nums[j];
+            nums[j] = val;
+        }
+        
+        if (nums[i] !== val) i++;
+        if (nums[j] === val) j--;
+    }    
+    
+    const ind = nums.indexOf(val) === -1 ? nums.length : nums.indexOf(val);
+    
+    return ind;
+};
