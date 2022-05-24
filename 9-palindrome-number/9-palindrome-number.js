8123bf90c15de9261ced9ad3df38d812f636b1c9
John Angcla <83146651+jangcla@users.noreply.github.com>
2022-05-23 23:53:33 -0700

    Time: 437 ms (6.61%), Space: 50.7 MB (77.80%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if (x < 0) return false;
+    
+    let num = "" + x;
+    let i = 0;
+    let j = num.length - 1;
+    
+    while (i < j) {
+        if (num[i] !== num[j]) return false;
+        
+        i++;
+        j--;
+    }
+    
+    return true;
+};
