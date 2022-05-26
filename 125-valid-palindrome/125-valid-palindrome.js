9d3d330d73a10f59672d9c2984caf0f56383ac0a
John Angcla <83146651+jangcla@users.noreply.github.com>
2022-05-26 10:31:59 -0700

    Time: 98 ms (52.18%), Space: 44.3 MB (82.84%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -3,11 +3,11 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    s = s.toLowerCase();
     const alphabet = new Set('abcdefghijklmnopqrstuvwxyz1234567890'.split(""));
+    s = s.toLowerCase();
+    
     let i = 0;
     let j = s.length - 1;
-    
     while (i < j) {
         let front = s[i];
         let back = s[j];
