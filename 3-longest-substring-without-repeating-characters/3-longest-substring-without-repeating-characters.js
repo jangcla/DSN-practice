df4903c0792114358a8738df3f30e2861985686f
John Angcla <83146651+jangcla@users.noreply.github.com>
2022-05-27 11:14:21 -0700

    Time: 1006 ms (5.55%), Space: 73.7 MB (5.01%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -2,53 +2,61 @@
  * @param {string} s
  * @return {number}
  */
-// const lengthOfLongestSubstring = (s) => {
-//     let charCheck = new Set();
-//     let longest = 0;
+const lengthOfLongestSubstring = (s) => {
+    let charCheck = {length: 0};
+    let longest = 0;
     
-//     for(let char of s) {
-//         if (charCheck.has(char)) charCheck = new Set();
-   
-//         charCheck.add(char);
-//         if (charCheck.size > longest) longest = charCheck.size;
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+        if (charCheck.length > longest) longest = charCheck.length;
+        
+        if (charCheck[char] === undefined) {
+            charCheck[char] = i;
+            charCheck.length++;
+        } else {
+            i = charCheck[char];
+            charCheck = {length: 0};
+        }
+        
+    }
+    
+    if (charCheck.length > longest) longest = charCheck.length;
+    return longest;
+}
+
+
+// var lengthOfLongestSubstring = function(s) {
+//     let subStrings = [];
+    
+//     let strObj = {};
+//     let tempStr = '';
+    
+//     let i = 0;
+//     let tempI = 0;
+//     while (i < s.length) {
+//         let letter = s[tempI];
+//         if (strObj[letter] === undefined) {
+//             strObj[letter] = 1;
+//             if (letter !== undefined) tempStr += letter;
+//         } else {
+//             subStrings.push(tempStr);
+            
+//             tempStr = "";
+//             strObj = {};
+            
+//             i++;
+//             tempI = i;
+//             continue;
+
+//         }
+//         tempI++;
 //     }
     
-//     return longest;
-// }
-
-
-var lengthOfLongestSubstring = function(s) {
-    let subStrings = [];
+//     let longestLength = 0;
     
-    let strObj = {};
-    let tempStr = '';
+//     for (let str of subStrings) {
+//         if (str.length > longestLength) longestLength = str.length;
+//     }
     
-    let i = 0;
-    let tempI = 0;
-    while (i < s.length) {
-        let letter = s[tempI];
-        if (strObj[letter] === undefined) {
-            strObj[letter] = 1;
-            if (letter !== undefined) tempStr += letter;
-        } else {
-            subStrings.push(tempStr);
-            
-            tempStr = "";
-            strObj = {};
-            
-            i++;
-            tempI = i;
-            continue;
-
-        }
-        tempI++;
-    }
-    
-    let longestLength = 0;
-    
-    for (let str of subStrings) {
-        if (str.length > longestLength) longestLength = str.length;
-    }
-    
-    return longestLength;
-};
+//     return longestLength;
+// };
