dc574c980ff7ffcf3bb010c3324f3942ddb7e85a
John Angcla <83146651+jangcla@users.noreply.github.com>
2022-05-27 21:14:21 -0700

    Time: 106 ms (37.56%), Space: 49.5 MB (13.69%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.js
+++ 64-minimum-path-sum/64-minimum-path-sum.js
@@ -9,8 +9,8 @@ var minPathSum = function(grid, row = 0, col = 0, memo = {}) {
     
     const pos = row + ',' + col;
     if (pos in memo) return memo[pos];
-    
     memo[pos] = grid[row][col] + Math.min(minPathSum(grid, row + 1, col, memo), minPathSum(grid, row, col + 1, memo));
+    
     return memo[pos];
 };
 
