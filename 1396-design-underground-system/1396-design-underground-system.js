f82acb8b6cedaabef3b9db427144fe01ed2797fc
John Angcla <83146651+jangcla@users.noreply.github.com>
2022-05-30 22:40:12 -0700

    Time: 533 ms (5.02%), Space: 60.4 MB (35.89%) - LeetHub

---
+++ 1396-design-underground-system/1396-design-underground-system.js
@@ -0,0 +1,66 @@
+
+var UndergroundSystem = function() {
+    this.train = {};
+    this.startStation = {};
+};
+
+/** 
+ * @param {number} id 
+ * @param {string} stationName 
+ * @param {number} t
+ * @return {void}
+ */
+UndergroundSystem.prototype.checkIn = function(id, stationName, t) {
+    if (this.startStation[id] === undefined) {
+        this.startStation[id] = [stationName, t];
+    } else {
+        this.startStation[id] = [stationName, t];
+    }
+};
+
+/** 
+ * @param {number} id 
+ * @param {string} stationName 
+ * @param {number} t
+ * @return {void}
+ */
+UndergroundSystem.prototype.checkOut = function(id, stationName, t) {
+    const checkInData = this.startStation[id];
+    const oldStation = checkInData[0];
+    const oldTime = checkInData[1];
+    
+    const distance = t - oldTime;
+    
+    // [oldStation, oldTime] = checkInData;
+    
+    if (this.train[`${oldStation} ${stationName}`] === undefined) {
+        this.train[`${oldStation} ${stationName}`] = [distance];
+    } else {
+        this.train[`${oldStation} ${stationName}`].push(distance);
+    }
+};
+
+/** 
+ * @param {string} startStation 
+ * @param {string} endStation
+ * @return {number}
+ */
+UndergroundSystem.prototype.getAverageTime = function(startStation, endStation) {
+    const distances = this.train[`${startStation} ${endStation}`];
+    
+    let totalDistances = 0;
+    distances.forEach((distance) => {
+        totalDistances += distance;
+    });
+    
+    const distanceAverage = (totalDistances / distances.length);
+    return distanceAverage;
+};
+
+/** 
+ * Your UndergroundSystem object will be instantiated and called as such:
+ * var obj = new UndergroundSystem()
+ * obj.checkIn(id,stationName,t)
+ * obj.checkOut(id,stationName,t)
+ * v  
+ */
