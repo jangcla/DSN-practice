e94ebb0bb80a9227eab6913b49ca71abcba48960
John Angcla <83146651+jangcla@users.noreply.github.com>
2022-06-03 10:55:58 -0700

    Time: 155 ms (20.66%), Space: 44.3 MB (81.83%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function(ransomNote, magazine) {
+    const letterCount = {};
+    for (let i = 0; i < magazine.length; i++) {
+        let char = magazine[i];
+        
+        if (char in letterCount) {
+            letterCount[char]++;
+        } else {
+            letterCount[char] = 1;
+        }
+    }
+    
+    for (let i = 0; i < ransomNote.length; i++) {
+        let char = ransomNote[i];
+        
+        if (char in letterCount) {
+            letterCount[char]--;
+        } else  {
+            return false;
+        }
+    }
+    
+    for (let char in letterCount) {
+        let value = letterCount[char];
+        
+        if (value < 0) return false;
+    }
+    
+    return true;
+};
