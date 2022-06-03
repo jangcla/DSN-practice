53b883aba026d219ac9a8a74a60ec8763f1c0afa
John Angcla <83146651+jangcla@users.noreply.github.com>
2022-06-03 11:19:46 -0700

    Time: 108 ms (29.52%), Space: 45.5 MB (23.06%) - LeetHub

---
+++ 169-majority-element/169-majority-element.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    const counter = {};
+    for (let num of nums) {
+        if (num in counter) counter[num]++;
+        else counter[num] = 1;
+    }
+    
+    let majority = nums[0];
+    for (let num in counter) {
+        if (counter[num] > counter[majority]) majority = num;
+    }
+    
+    return majority;
+};
