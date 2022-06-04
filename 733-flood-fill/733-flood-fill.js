7541c2e74453e6c2ace1d0c01f1f185358c58a29
John Angcla <83146651+jangcla@users.noreply.github.com>
2022-06-03 19:37:07 -0700

    Time: 129 ms (17.53%), Space: 44.7 MB (26.23%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[][]} image
+ * @param {number} sr
+ * @param {number} sc
+ * @param {number} newColor
+ * @return {number[][]}
+ */
+var floodFill = function(image, sr, sc, newColor, original = image[sr][sc]) {
+    const rowInbound = sr >= 0 && sr < image.length;
+    const colInbound = sc >= 0 && sc < image[0].length;
+    if (!rowInbound || !colInbound) return image;
+    if (image[sr][sc] === newColor) return image;
+    if (image[sr][sc] !== original) return image;
+    
+    image[sr][sc] = newColor;
+    
+    floodFill(image, sr + 1, sc, newColor, original);
+    floodFill(image, sr - 1, sc, newColor, original);
+    floodFill(image, sr, sc + 1, newColor, original);
+    floodFill(image, sr, sc - 1, newColor, original);
+    
+    return image; 
+};
