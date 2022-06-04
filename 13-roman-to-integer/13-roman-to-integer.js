3614cf4b0b6e1d1e54022811e93031e848137b27
John Angcla <83146651+jangcla@users.noreply.github.com>
2022-06-03 20:55:58 -0700

    Time: 141 ms (79.38%), Space: 46.9 MB (77.87%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    const roman = {
+        "I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000
+    };
+    
+    let num = 0;
+    for (let i = 0; i < s.length; i++) {
+        const current = s[i];
+        const next = s[i + 1];
+        
+        if (current === "I") {
+            if (next === "V" || next === "X"){
+                num -= roman[current];
+                continue;
+            } 
+        } else if (current === "X") {
+            if (next === "L" || next === "C") {
+                num -= roman[current];
+                continue;
+            }
+        } else if (current === "C") {
+            if (next === "D" || next === "M"){
+                num -= roman[current];
+                continue;
+            } 
+        }
+          
+        num += roman[current];
+    }
+    
+    return num;
+};
