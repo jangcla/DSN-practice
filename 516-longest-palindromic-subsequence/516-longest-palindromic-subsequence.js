4d33883440fd123d483fb55f0708ae8568b5064f
John Angcla <83146651+jangcla@users.noreply.github.com>
2022-06-07 23:42:28 -0700

    Time: 4471 ms (5.04%), Space: 175.8 MB (6.12%) - LeetHub

---
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindromeSubseq = function(s, i = 0, j = s.length, memo = {}) {
+    const key = i + ',' + j;
+    if (key in memo) return memo[key];
+    
+    if (i === j) return 1;
+    if (i > j) return 0;
+    
+    if (s[i] === s[j]) {
+        memo[key] = 2 + longestPalindromeSubseq(s, i + 1, j - 1, memo);    
+    } else {
+        memo[key] = Math.max(longestPalindromeSubseq(s, i + 1, j, memo),longestPalindromeSubseq(s, i, j - 1, memo))
+    }
+    
+    return memo[key];
+};
