8328fa6674bcb939eb3b076b3ce31a8e15de08b7
John Angcla <83146651+jangcla@users.noreply.github.com>
2022-06-09 11:21:58 -0700

    Time: 117 ms (13.70%), Space: 43.1 MB (40.98%) - LeetHub

---
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let i = 0;
+    let j = numbers.length - 1;
+    
+    while (i < j) {
+        const result = numbers[i] + numbers[j];
+        if (result === target) return [i + 1, j + 1];
+        
+        if (result < target) {
+            i++;
+        } else {
+            j--;
+        }
+    }
+};
