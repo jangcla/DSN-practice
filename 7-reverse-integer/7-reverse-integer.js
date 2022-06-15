48263c29f4b5e1e73c4b49e768d4dc8d38b00aae
John Angcla <83146651+jangcla@users.noreply.github.com>
2022-06-14 22:11:55 -0700

    Time: 128 ms (19.42%), Space: 44.6 MB (12.30%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    let num = x;
+    if (x < 0) num = num * -1;
+    let number = String(num).split("").map((ele) => Number(ele));
+    
+    while (number[number.length - 1] === 0) {
+        number.pop();
+    }
+    
+    number = Number(number.reverse().join(''));
+    
+    if (number > Math.pow(2, 31)) return 0;
+    if (x < 0) return number * -1;
+    return number;
+};
