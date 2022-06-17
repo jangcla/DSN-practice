7b659ccfac45b571b237382d643608256d608d39
John Angcla <83146651+jangcla@users.noreply.github.com>
2022-06-16 17:03:42 -0700

    Time: 100 ms (33.20%), Space: 44.7 MB (92.15%) - LeetHub

---
+++ 704-binary-search/704-binary-search.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let low = 0;
+    let high = nums.length - 1;
+    
+    let mid;
+    
+    while (low <= high) {
+        mid = Math.floor((low + high) / 2);       
+        if (nums[mid] === target) return mid;
+        
+        if (nums[mid] > target) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    
+    return -1;
+};
