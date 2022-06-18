c53514e0424e8cc576e338d7351aab103b527009
John Angcla <83146651+jangcla@users.noreply.github.com>
2022-06-17 22:17:16 -0700

    Time: 71 ms (73.22%), Space: 42 MB (63.70%) - LeetHub

---
+++ 66-plus-one/66-plus-one.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    let carry = 1;
+    
+    for (let i = digits.length - 1; i >= 0; i--) {
+        let num = digits[i];
+        
+        if (num + carry > 9) {
+            digits[i] = 0;
+            carry = 1;
+        } else {
+            digits[i] += carry;
+            carry = 0;
+        }
+    }
+    
+    if (carry === 1) digits.unshift(1);
+    
+    return digits;
+};
