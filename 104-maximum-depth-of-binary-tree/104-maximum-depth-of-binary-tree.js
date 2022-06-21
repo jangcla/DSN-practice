23a91659bdb77ff526f4b2878ec1c669b33ec89c
John Angcla <83146651+jangcla@users.noreply.github.com>
2022-06-20 23:22:27 -0700

    Time: 102 ms (41.83%), Space: 45.2 MB (51.98%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
@@ -10,18 +10,29 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var maxDepth = function(root) {
-    if (!root) return 0;
-    
-    const stack = [[root, 1]];
-    let depth = 0;
-    while (stack.length > 0) {
-        const [node, level] = stack.pop();
-        depth = Math.max(level, depth);
-        
-        if (node.left) stack.push([node.left, level + 1]);
-        if (node.right) stack.push([node.right, level + 1]);
-    }
-    
-    return depth;
+// RECURSIVE ATTEMPT
+const maxDepth = (root) => {
+    if (!root) return 0;    
+    if (!root.left && !root.right) return 1;
+
+    return Math.max(1 + maxDepth(root.right), 1 + maxDepth(root.left));
 };
+
+
+
+// ITERATIVE ATTEMPT
+// var maxDepth = function(root) {
+//     if (!root) return 0;
+    
+//     const stack = [[root, 1]];
+//     let depth = 0;
+//     while (stack.length > 0) {
+//         const [node, level] = stack.pop();
+//         depth = Math.max(level, depth);
+        
+//         if (node.left) stack.push([node.left, level + 1]);
+//         if (node.right) stack.push([node.right, level + 1]);
+//     }
+    
+//     return depth;
+// };
