13a745edfe82237dea4679c692ae905cd2d09c11
John Angcla <83146651+jangcla@users.noreply.github.com>
2022-06-20 23:17:24 -0700

    Time: 113 ms (25.68%), Space: 46.6 MB (5.73%) - LeetHub

---
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if (!root) return 0;
+    
+    const stack = [[root, 1]];
+    let depth = 0;
+    
+    while (stack.length > 0) {
+        const [node, level] = stack.pop();
+        depth = Math.max(level, depth);
+        
+        if (node.left) stack.push([node.left, level + 1]);
+        if (node.right) stack.push([node.right, level + 1]);
+    }
+    
+    return depth;
+};
