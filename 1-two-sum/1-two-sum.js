ede79e03ad18c224465eb8a194c7e829f02943e6
John Angcla <83146651+jangcla@users.noreply.github.com>
2022-07-15 11:20:08 -0700

    Time: 90 ms (78.95%), Space: 43.3 MB (26.58%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,15 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let complements = {};
+    const complements = {};
     
     for (let i = 0; i < nums.length; i++) {
-        let num = nums[i];
-        let complement = target - num;
+        const num = nums[i];
+        const complement = target - num;
         
         if (complements[num] !== undefined) return [complements[num], i];
         complements[complement] = i;
     }
 };
 
-//this is O(n2)
