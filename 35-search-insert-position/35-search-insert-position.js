0267fd92aaedecdd7be5ec09ffcdab2b6ca2d213
John Angcla <83146651+jangcla@users.noreply.github.com>
2022-08-22 23:01:51 -0700

    Time: 72 ms (79.16%), Space: 42 MB (74.46%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    if (!nums.length) return 0;
+    
+    let half = Math.floor(nums.length / 2);
+    if (nums[half] === target) return half;
+    
+    if (nums[half] > target) {
+        return searchInsert(nums.slice(0, half), target);
+    } else {
+        return 1 + half + searchInsert(nums.slice(half + 1), target);
+    }
+};
